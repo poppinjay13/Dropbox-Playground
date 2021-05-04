868cc672e4058d951c61aa71a7e6b9c948acaf89
Ian Odundo <thisian13@gmail.com>
2021-05-04 20:59:02 +0300

    initial connection test

--- app/src/main/java/com/poppinjay13/dropboxplayground/ScrollingActivity.java
+++ app/src/main/java/com/poppinjay13/dropboxplayground/ScrollingActivity.java
@@ -1,17 +1,21 @@
 package com.poppinjay13.dropboxplayground;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.users.FullAccount;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -25,12 +29,28 @@ public class ScrollingActivity extends AppCompatActivity {
         toolBarLayout.setTitle(getTitle());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        fab.setOnClickListener(view -> {
+            /*
+            I'm using a thread coz I don't want to perform network requests on the main thread
+            It is more advisable to use an AsyncTask or a Service / IntentService if you will be making regular requests
+            And also to avoid memory leaks but I'm lazy.
+            */
+            Thread thread = new Thread(() -> {
+                try {
+                    DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
+                    DbxClientV2 client = new DbxClientV2(config, Config.ACCESS_TOKEN);
+                    // Get current user account info
+                    FullAccount account = client.users().getCurrentAccount();
+                    Snackbar.make(view, account.getName().getDisplayName(), Snackbar.LENGTH_LONG).show();
+                } catch (DbxException ex) {
+                    Snackbar.make(view, "A dropbox error occurred", Snackbar.LENGTH_LONG).show();
+                    ex.printStackTrace();
+                } catch (Exception ex) {
+                    Snackbar.make(view, "An error occurred", Snackbar.LENGTH_LONG).show();
+                    ex.printStackTrace();
+                }
+            });
+            thread.start();
         });
     }
 
@@ -48,9 +68,10 @@ public class ScrollingActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.developer) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://poppinjay13.github.io/")));
         }
         return super.onOptionsItemSelected(item);
     }
